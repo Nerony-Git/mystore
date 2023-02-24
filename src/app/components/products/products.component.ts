9f6b1f157b91666341c9e518495047b6fa05a0ef
George Amuzu <gnamuzu@gmail.com>
2023-02-24 22:21:37 +0000

    updated to include routing

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import Product from 'src/app/models/product';
+import { Subscription } from 'rxjs';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
+  private productData!: Subscription;
+  products!: Product[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productData = this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.productData.unsubscribe();
+  }
 
 }
