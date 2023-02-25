bf119ee21f59ef420b23ae26158474950147fece
George Amuzu <gnamuzu@gmail.com>
2023-02-25 20:16:58 +0000

    updated

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,11 +1,33 @@
 import { Component, Input } from '@angular/core';
 import Product from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
+import { AddToCartComponent } from 'src/app/modals/add-to-cart/add-to-cart.component';
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  styleUrls: ['./products-list.component.css'],
+  providers: [MdbModalService]
 })
 export class ProductsListComponent {
   @Input() product!: Product;
+  quantity: number = 0;
+  modalRef: MdbModalRef<AddToCartComponent> | null = null;
+
+  constructor(
+    private cartService: CartService,
+    private modalService: MdbModalService
+    ) {}
+
+  addToCart(): void {
+    this.cartService.addProducts(this.product, this.quantity);
+    this.quantity = 0;
+  }
+
+  openModal() {
+    this.modalRef = this.modalService.open(AddToCartComponent, {
+      data: { title: 'Added to Cart'},
+    });
+  }
 }
