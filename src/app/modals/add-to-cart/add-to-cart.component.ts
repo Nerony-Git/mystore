613144dc960262438bed52922a75e58ce28d2506
George Amuzu <gnamuzu@gmail.com>
2023-02-25 20:16:13 +0000

    added modal component

---
+++ src/app/modals/add-to-cart/add-to-cart.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
+
+@Component({
+  selector: 'app-add-to-cart',
+  templateUrl: './add-to-cart.component.html',
+  styleUrls: ['./add-to-cart.component.css']
+})
+export class AddToCartComponent {
+  title: string | null = null;
+
+  constructor(public modalRef: MdbModalRef<AddToCartComponent>){}
+
+}
