125d1d53ccc75601aef120af7ebcd8a75f6f93a2
George Amuzu <gnamuzu@gmail.com>
2023-02-27 13:15:15 +0000

    resolved back-to-top issues

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  windowScrolled = false;
+
+  constructor(){}
+
+  scrollToTop(): void {
+    window.scrollTo(0, 0);
+  }
+
+  ngOnInit(): void {
+    window.addEventListener('scroll', () => {
+      this.windowScrolled = window.pageYOffset !== 0;
+    });
+  }
 
 }
