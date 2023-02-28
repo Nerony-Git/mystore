8aea1599c1d98dbc183b1ef8ea4c0298845c1717
George Amuzu <gnamuzu@gmail.com>
2023-02-28 14:48:43 +0000

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { ProductsComponent } from './components/products/products.component';
 import { CartComponent } from './components/cart/cart.component';
 import { OrdersComponent } from './components/orders/orders.component';
+import { FaqsComponent } from './components/faqs/faqs.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { TncsComponent } from './components/tncs/tncs.component';
 
 const routes: Routes = [
   { path: '', component: ProductsComponent },
-  {path: 'product/:id', component: ProductDetailsComponent},
-  {path: 'cart', component: CartComponent},
-  {path: 'order', component: OrdersComponent},
-  {path: '**', redirectTo: '/'}
+  { path: 'product/:id', component: ProductDetailsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'order', component: OrdersComponent },
+  { path: 'faqs', component: FaqsComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'tncs', component: TncsComponent },
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
