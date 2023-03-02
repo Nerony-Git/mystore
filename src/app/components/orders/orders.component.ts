c92a22a09ad460a939cd34803519fd24f49d16fa
George Amuzu <gnamuzu@gmail.com>
2023-03-02 07:25:00 +0000

    list the ordered items.

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
 import Order from 'src/app/models/order';
+import CartProduct from 'src/app/models/cart-product';
 
 @Component({
   selector: 'app-orders',
@@ -9,10 +10,18 @@ import Order from 'src/app/models/order';
 })
 export class OrdersComponent implements OnInit {
   orderDetails!: Order;
+  orderProducts: CartProduct[] = [];
+
 
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
     this.orderDetails = this.cartService.getOrder();
+    if (this.orderDetails?.cart?.items !== null){
+      this.orderProducts = Object
+      .keys(this.orderDetails.cart.items)
+      .map((key) => (this.orderDetails.cart.items![parseInt(key)]));
+    }
+
   }
 }
